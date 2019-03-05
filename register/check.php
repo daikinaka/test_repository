70c96a065797c6006bb2dee4adb6491f26526564
中塩大毅 <>
2019-03-05 15:56:17 +0800

    3月５日コミットしまーす！

--- register/check.php
+++ register/check.php
@@ -1,4 +1,21 @@
 <?php
+session_start();
+
+// 不正遷移制御
+// sign.phpから来ていない場合、signup.phpに強制遷移させる
+if (!isset($_SESSION['51_LearnSNS'])) {
+    header('Location: signup.php');
+    exit();
+}
+
+// echo '<pre>';
+// var_dump($_SESSION);
+// echo '</pre>';
+
+$name = $_SESSION['51_LearnSNS']['name'];
+$email = $_SESSION['51_LearnSNS']['email'];
+$password = $_SESSION['51_LearnSNS']['password'];
+$img_name = $_SESSION['51_LearnSNS']['img_name'];
 
 ?>
 <!DOCTYPE html>
@@ -16,16 +33,16 @@
                 <h2 class="text-center content_header">アカウント情報確認</h2>
                 <div class="row">
                     <div class="col-xs-4">
-                        <img src="../user_profile_img/misae.png" class="img-responsive img-thumbnail">
+                        <img src="../user_profile_img/<?php echo $img_name; ?>" class="img-responsive img-thumbnail">
                     </div>
                     <div class="col-xs-8">
                         <div>
                             <span>ユーザー名</span>
-                            <p class="lead">野原みさえ</p>
+                            <p class="lead"><?php echo htmlspecialchars($name); ?></p>
                         </div>
                         <div>
                             <span>メールアドレス</span>
-                            <p class="lead">misae@nohara.com</p>
+                            <p class="lead"><?php echo htmlspecialchars($email); ?></p>
                         </div>
                         <div>
                             <span>パスワード</span>
--- register/signup.php
+++ register/signup.php
@@ -1,10 +1,24 @@
 <?php
+
+
+session_start();
 // バリデーション
 // 入力値が正しく設定されているかを確認し、不正な場合はユーザーに再入力を促す機能
 
 $errors = [];
 
+//check.phpから戻るできた場合
+if (isset($_GET['action']) && $_GET['action'] == 'rewrite') {
+    $_POST['input_name'] = $_SESSION['51_LearnSNS']['name'];
+    $_POST['input_email'] = $_SESSION['51_LearnSNS']['email'];
+    $_POST['input_password'] = $_SESSION['51_LearnSNS']['password'];
+    $errors['rewrite'] = true;
+}
+
 // POST送信時
+$name = '';
+$email = '';
+
 if (!empty($_POST)) {
     $name = $_POST['input_name'];
     $email = $_POST['input_email'];
@@ -28,13 +42,52 @@ if (!empty($_POST)) {
     // ファイルに関しては$_FILESというスーパーグローバル変数を使用
     // $_FILESを利用するための決まりごと
 
-    $file_name = $_FILES['input_img_name']['name'];
-    if (file_name != '') {
-        $file_name != '';
+    $file_name = '';
+    if (!isset($_GET['action'])) {
+        $file_name = $_FILES['input_img_name']['name'];
+    }
+
+    if ($file_name != '') {
+        //拡張子のチェック
+        $file_type = substr($file_name, -3);
+        // 後ろから三文字を取り出した
+        $file_type = strtolower($file_type);
+        // 小文字にして再代入
+        if ($file_type != 'jpg' && $file_type != 'png' && $file_type != 'gif') {
+            $errors['img_name'] = 'type';
+        }
     } else {
         $errors['img_name'] = 'blank';
     }
+    if (empty($errors)) {
+        // 画像のアップロード
+        // 画像自体はフォルダに保存
+        // 画像のパスはDBに保存
+        // 2019030514052529.png
+        // 現在日時分秒を取得
+        $date_str = date('YmdHis');
+
+        // タイムスタンプとファイル名を結合して一意のファイル名を生成する
+        $submit_file_name = $date_str.$file_name;
+
+        move_uploaded_file($_FILES['input_img_name']['tmp_name'], '../user_profile_img/'.$submit_file_name);
+        // ../は一個上のフォルダという意味
+
+        // セッションに送信されたデータを保存
+        // PHPでは$_SESSIONを利用する
+        // セッションに値を格納
+        // 連想配列形式
+        $_SESSION['51_LearnSNS']['name'] = $name;
+        $_SESSION['51_LearnSNS']['email'] = $email;
+        $_SESSION['51_LearnSNS']['password'] = $password;
+        $_SESSION['51_LearnSNS']['img_name'] = $submit_file_name;
+
+        // 画面遷移
+        header('Location: check.php');
+        exit();
+    }
 }
+
 // フォーム送信された値の取得
 
 ?>
@@ -65,7 +118,7 @@ if (!empty($_POST)) {
                     <div class="form-group">
                         <label for="name">ユーザー名</label>
                         <input type="text" name="input_name" class="form-control" id="name" placeholder="山田 太郎"
-                            value="">
+                            value="<?php echo $name; ?>">
                         <?php if (isset($errors['name']) && $errors['name'] == 'blank'): ?>
                         <p class="text-danger">ユーザー名を入力してください</p>
                         <?php endif; ?>
@@ -73,10 +126,11 @@ if (!empty($_POST)) {
                     <div class="form-group">
                         <label for="email">メールアドレス</label>
                         <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com"
-                            value="">
+                            value="<?php echo $email; ?>">
                         <?php if (isset($errors['email']) && $errors['email'] == 'blank'): ?>
                         <p class="text-danger">emailを入力してください</p>
                         <?php endif; ?>
+                        
                     </div>
                     <div class="form-group">
                         <label for="password">パスワード</label>
@@ -87,12 +141,19 @@ if (!empty($_POST)) {
                         <?php if (isset($errors['password']) && $errors['password'] == 'length'): ?>
                         <p class="text-danger">パスワードは4-16で入力してください</p>
                         <?php endif; ?>
+                        <?php if(!empty($errors)): ?>
+                        <p class = "text-danger">パスワードを再度入力してください</p>
+                        <?php endif；； ?>
+                        
                     </div>
                     <div class="form-group">
                         <label for="img_name">プロフィール画像</label>
                         <input type="file" name="input_img_name" id="img_name" accept="image/*">
                         <?php if (isset($errors['img_name']) && $errors['img_name'] == 'blank'): ?>
                         <p class="text-danger">画像をアップロードしてください</p>
+                        <?php endif; ?>
+                        <?php if (isset($errors['img_name']) && $errors['img_name'] == 'type'): ?>
+                        <p class = "text-danger">拡張子がjpg,png,gifの画像を選択してください</p>
                         <?php endif; ?>
                     </div>
                     <input type="submit" class="btn btn-default" value="確認">
