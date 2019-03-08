1c9fd5cfe665fc3273b727b657e886802c3bab38
中塩大毅 <>
2019-03-08 13:35:45 +0800

    comit

--- signout.php
+++ signout.php
@@ -1,4 +1,14 @@
 <?php
 
-header("Location: signin.php");
+session_start();
+
+// セッションにユーザーのIDが保持されtいたらサインインしている状態
+// セッション変数の破棄
+// ブラウザから破棄
+$_SESSION = [];
+
+// サーバー内のセッション変数のクリア
+session_destroy();
+
+header('Location: signin.php');
 exit();
